f9ff29dde8d2b975c71449d0405a95c5f8082d75
Tobibur Rahman <tobiburrahman786@gmail.com>
2020-01-25 23:15:29 +0530

    added pagination

--- app/src/main/java/com/tobibur/themoviefinderapp/ui/MoviesFragment.kt
+++ app/src/main/java/com/tobibur/themoviefinderapp/ui/MoviesFragment.kt
@@ -2,27 +2,28 @@ package com.tobibur.themoviefinderapp.ui
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
-
+import com.tobibur.pagination.PageListener
+import com.tobibur.pagination.PaginationUtils
 import com.tobibur.themoviefinderapp.R
-import com.tobibur.themoviefinderapp.data.model.Movie
 import com.tobibur.themoviefinderapp.data.repository.Outcome
 import kotlinx.android.synthetic.main.movies_fragment.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class MoviesFragment : Fragment() {
+class MoviesFragment : Fragment(), PageListener {
 
     companion object {
         fun newInstance() = MoviesFragment()
         private const val TAG = "MoviesFragment"
     }
 
+    private lateinit var moviesAdapter: MoviesAdapter
     private val moviesViewModel: MoviesViewModel by viewModel()
 
     override fun onCreateView(
@@ -35,24 +36,36 @@ class MoviesFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val moviesAdapter = MoviesAdapter(mutableListOf())
+        moviesAdapter = MoviesAdapter(mutableListOf())
         val gridLayoutManager = GridLayoutManager(activity, 3)
         recycler_movies.layoutManager = gridLayoutManager
         recycler_movies.setHasFixedSize(true)
         recycler_movies.adapter = moviesAdapter
+        PaginationUtils.initPagination(
+            recycler_movies, gridLayoutManager,
+            this
+        )
 
-        moviesViewModel.popularMovies(1).observe(viewLifecycleOwner, Observer { outcome->
+        getPopularMovies()
+    }
+
+    private fun getPopularMovies(page: Int = 1) {
+        moviesViewModel.popularMovies(page).observe(viewLifecycleOwner, Observer { outcome ->
             progress_movies.visibility = View.GONE
-            when(outcome){
+            when (outcome) {
                 is Outcome.Success -> {
                     Log.d(TAG, "onActivityCreated: ${outcome.data}")
-                    Toast.makeText( activity!!, "Success!", Toast.LENGTH_SHORT).show()
+                    outcome.data.totalPages
                     moviesAdapter.addMovies(outcome.data.results)
                 }
                 is Outcome.Failure -> {
-                    Toast.makeText( activity!!, outcome.e.message, Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity!!, outcome.e.message, Toast.LENGTH_SHORT).show()
                 }
             }
         })
+    }
+
+    override fun onPagination(page: Int) {
+        getPopularMovies(page)
     }
 }
